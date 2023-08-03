c8323abdf55ee9d74a71bc6c5c5e4b7b60077858
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-08-03 03:48:06 +0100

    login, download file test, get data test

--- main.py
+++ main.py
@@ -1,23 +1,69 @@
-"""
-Clipper
-A simple python web scrapper for FCT-NOVA's own e-learning platform, Clip.
-"""
-
-__author__ = "Afonso Bras Sousa"
-__maintainer__ = "Afonso Bras Sousa"
-__email__ = "ab.sousa@campus.fct.unl.pt"
-__version__ = 0.1
-
 import requests
-from bs4 import BeautifulSoup as bs
 import re
+import getpass
+from bs4 import BeautifulSoup as bs
+from tqdm import tqdm
 
-def get_html(user, password, url):
-  form_data = {'identificador': user, 'senha': password}
-  response = requests.post("https://clip.fct.unl.pt/utente/eu/aluno/ano_lectivo/unidades/unidade_curricular/actividade/documentos?tipo_de_per%EDodo_lectivo=s&tipo_de_documento_de_unidade=t&ano_lectivo=2023&per%EDodo_lectivo=1&unidade_curricular=11504", data=form_data)
-  soup = bs(response.text, 'html.parser')
+class LoginError(Exception):
+    "Raised when the login fails"
+    def __init__(self, message="Login error"):
+        print(message)
 
-links = []
-for link in soup.find_all('a', href=re.compile("^/objecto*")):
-  links.append(link.get('href'))
-print(links)
+def getLogin():
+    username = input("Username: ")
+    password = getpass.getpass()
+
+    login_data = {
+        'identificador': username,
+        'senha': password
+    }
+
+    with requests.Session() as session:
+        try:
+            response = session.post('https://clip.fct.unl.pt/', data=login_data)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            if "Autenticação inválida" in response.text:
+                raise LoginError("Autenticação falhou.")
+        except requests.exceptions.RequestException as e:
+            raise LoginError("Erro de conexão durante o login.")
+
+        return session
+
+def get_html(url, session):
+    response = session.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    return response.text
+
+def download(url, session):
+    response = session.get(url, stream=True)
+    with open("/tmp/1", "wb") as file:
+        for data in tqdm(response.iter_content()):
+            file.write(data)
+
+def main():
+    valid_login = False
+    while not valid_login:
+        try:
+            login = getLogin()
+            valid_login = True
+        except LoginError:
+            continue
+
+    ano_lectivo=2023
+    semestre=1
+    unidade_curricular=11504
+    tipo_documento='t'
+    domain='https://clip.fct.unl.pt'
+
+    url = f'{domain}/utente/eu/aluno/ano_lectivo/unidades/unidade_curricular/actividade/documentos?tipo_de_per%EDodo_lectivo=s&tipo_de_documento_de_unidade={tipo_documento}&ano_lectivo={ano_lectivo}&per%EDodo_lectivo={semestre}&unidade_curricular={unidade_curricular}'
+    soup = bs(get_html(url, login), 'html.parser')
+
+    links = []
+    for link in soup.find_all('a', href=re.compile("^/objecto*")):
+        links.append(domain+link.get('href'))
+
+    print(links)
+    download(links[1],login)
+
+if __name__ == "__main__":
+    main()
