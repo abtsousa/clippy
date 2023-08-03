ec6127ad0fb2b757ba1482dabc0ba0c6f8cfbb68
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-08-03 01:09:08 +0100

    start

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
-import requests, sys, getpass, pandas
+"""
+Clipper
+A simple python web scrapper for FCT-NOVA's own e-learning platform, Clip.
+"""
 
+__author__ = "Afonso Bras Sousa"
+__maintainer__ = "Afonso Bras Sousa"
+__email__ = "ab.sousa@campus.fct.unl.pt"
+__version__ = 0.1
 
-class LoginError(Exception):
-    "Raised when the login fails"
-    print("Autenticação falhou.")
-    pass
+import requests
+from bs4 import BeautifulSoup as bs
+import re
 
+def get_html(user, password, url):
+  form_data = {'identificador': user, 'senha': password}
+  response = requests.post("https://clip.fct.unl.pt/utente/eu/aluno/ano_lectivo/unidades/unidade_curricular/actividade/documentos?tipo_de_per%EDodo_lectivo=s&tipo_de_documento_de_unidade=t&ano_lectivo=2023&per%EDodo_lectivo=1&unidade_curricular=11504", data=form_data)
+  soup = bs(response.text, 'html.parser')
 
-def getLogin():
-    print('Username:')
-    username = input()
-    password = getpass.getpass()
-
-    # Fill in your details here to be posted to the login form.
-    payload = {
-        'identificador': username,
-        'senha': password
-    }
-
-    return payload
-
-
-# Use 'with' to ensure the session context is closed after use.
-with requests.Session() as s:
-    while True:
-        try:
-            p = s.post('https://clip.fct.unl.pt/', data=getLogin())
-            if "Autenticação inválida" in p.text:
-                raise LoginError("Autenticação falhou.")
-            break
-        except LoginError:
-            pass
-
-    #TODO https://clip.fct.unl.pt/utente/eu -> aluno -> ano lectivo -> unidades curriculares por semestre
-
-    # An authorised request.
-    r = s.get('https://clip.fct.unl.pt/utente/eu/aluno/ano_lectivo/unidades?tipo_de_per%EDodo_lectivo=s&ano_lectivo=2023&per%EDodo_lectivo=1&aluno=118029&institui%E7%E3o=97747&unidade_curricular=11505')
-    r.raise_for_status()
-
-    tb_list = pandas.read_html(r.text)
-    files = tb_list[9]
-    print(files.loc[0])
-    print("Finished.")
-    
+links = []
+for link in soup.find_all('a', href=re.compile("^/objecto*")):
+  links.append(link.get('href'))
+print(links)
