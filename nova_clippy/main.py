fd01dae4334af4563259079260a3fc5c2375de2d
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-09-15 00:26:43 +0100

    new icon

--- nova_clippy/main.py
+++ nova_clippy/main.py
@@ -62,7 +62,7 @@ with a similar structure, keeping it in sync with the server.
 __author__ = "Afonso Bras Sousa (LEI-65263)"
 __maintainer__ = "Afonso Bras Sousa"
 __email__ = "ab.sousa@campus.fct.unl.pt"
-__version__ = "0.9b2"
+__version__ = "0.9.3"
 
 app = typer.Typer()
 
